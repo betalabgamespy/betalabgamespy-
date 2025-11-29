5bf6915f2444eb2c0c30f599cbebab1987ed37d0
betalabgamespy <betalabgamespy@gmail.com>
2025-11-29 11:07:24 -0300

    mejors

--- PEDIDOS/formulario.js
+++ PEDIDOS/formulario.js
@@ -177,7 +177,7 @@ function enviarAGmail(event) {
     cuerpoMensaje += `📎 IMPORTANTE: El cliente confirma que realizará/habrá realizado la transferencia y adjuntará el comprobante en este correo.`;
 
     // Enviar por Gmail
-    const emailDestino = 'betalabgamespy@gmail.com';
+    const emailDestino = 'betalabgamespedidos@gmail.com';
     const asunto = `🎮 PEDIDO - ${nombre} ${apellido}`;
     const mailtoLink = `mailto:${emailDestino}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpoMensaje)}`;
     
@@ -217,3 +217,4 @@ function actualizarMontoTransferencia(precio) {
 // Hacer funciones globales
 window.vaciarCarrito = vaciarCarrito;
 window.enviarAGmail = enviarAGmail;
+
